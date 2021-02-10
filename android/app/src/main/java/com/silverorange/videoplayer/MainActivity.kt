d451a19fe3fcb485371c6c108e3f0439ac5f7893
Sai Vikranth Desu <vikranthdesu@gmail.com>
2021-02-09 20:25:35 -0500

    Build basic UI components layout

--- android/app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
+++ android/app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
@@ -3,13 +3,21 @@ package com.silverorange.videoplayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.google.android.exoplayer2.SimpleExoPlayer
 import com.silverorange.videoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
 
+    private lateinit var player: SimpleExoPlayer
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Bind layout
         bind = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        // Initialize Player
+        player = SimpleExoPlayer.Builder(this).build()
+        bind.player.player = player
     }
 }
