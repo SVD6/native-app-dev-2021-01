43a64c9e60842cbcc8adf75d01eaece48e9c15e4
Sai Vikranth Desu <vikranthdesu@gmail.com>
2021-02-10 15:52:01 -0500

    Add control buttons and functionality

--- android/app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
+++ android/app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
@@ -3,13 +3,18 @@ package com.silverorange.videoplayer
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Toast
 import androidx.annotation.RequiresApi
 import androidx.databinding.DataBindingUtil
+import com.google.android.exoplayer2.MediaItem
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.google.gson.GsonBuilder
 import com.silverorange.videoplayer.databinding.ActivityMainBinding
 import com.silverorange.videoplayer.util.objects.Author
 import com.silverorange.videoplayer.util.objects.VideoObject
+import io.noties.markwon.Markwon
 import org.json.JSONArray
 import java.time.format.DateTimeFormatter
 import kotlin.collections.ArrayList
@@ -18,8 +23,11 @@ class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
 
     private lateinit var player: SimpleExoPlayer
+    private lateinit var markwon: Markwon
 
     private lateinit var videos: MutableList<VideoObject>
+
+    private var showControls = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,17 +38,43 @@ class MainActivity : AppCompatActivity() {
         player = SimpleExoPlayer.Builder(this).build()
         bind.player.player = player
 
+        // Initialize Markdown
+        markwon = Markwon.create(this)
+
         // Load Videos
         videos = ArrayList()
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            loadVideos(intent.getStringExtra("jsonData")!!)
+            loadData(intent.getStringExtra("jsonData")!!)
         }
 
+        bind.playPause.setOnClickListener {
+            if (player.isPlaying) {
+                player.pause()
+                bind.playPause.setImageResource(R.drawable.ic_play)
+            } else {
+                player.play()
+                bind.playPause.setImageResource(R.drawable.ic_pause)
+            }
+        }
 
+        bind.playerLayout.setOnClickListener {
+            Toast.makeText(this, "Clicked", Toast.LENGTH_SHORT).show()
+            if (showControls) {
+                bind.playPause.visibility = View.GONE
+                bind.next.visibility = View.GONE
+                bind.previous.visibility = View.GONE
+                showControls = false
+            } else {
+                bind.playPause.visibility = View.VISIBLE
+                bind.next.visibility = View.VISIBLE
+                bind.previous.visibility = View.VISIBLE
+                showControls = true
+            }
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.O)
-    private fun loadVideos(jsonData: String) {
+    private fun loadData(jsonData: String) {
         val jsonArray = JSONArray(jsonData)
         for (i in 0 until jsonArray.length()) {
             val jsonObject = jsonArray.getJSONObject(i)
@@ -61,6 +95,22 @@ class MainActivity : AppCompatActivity() {
         videos.sortBy { it.publishedAt }
 
         // Load First Video
+        loadVideo(videos[0])
 
+        // Attempt Margin fix
+        val params = bind.playPause.layoutParams as ViewGroup.MarginLayoutParams
+        val playerHeight = bind.player.height
+        params.setMargins(0, playerHeight / 2, 0, 0)
+        bind.playPause.layoutParams = params
     }
+
+    private fun loadVideo(video: VideoObject) {
+        player.setMediaItem(MediaItem.fromUri(video.hlsURL!!))
+        player.prepare()
+
+        bind.videoTitle.text = video.title
+        bind.videoAuthor.text = video.author!!.name
+        markwon.setMarkdown(bind.videoDetails, video.description!!)
+    }
+
 }
