b3f201824c1b44e06c43d8b99e84f914d66e161c
Sai Vikranth Desu <vikranthdesu@gmail.com>
2021-02-10 11:52:23 -0500

    Fix server connection issues

--- android/app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
+++ android/app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
@@ -2,14 +2,22 @@ package com.silverorange.videoplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import androidx.databinding.DataBindingUtil
 import com.google.android.exoplayer2.SimpleExoPlayer
 import com.silverorange.videoplayer.databinding.ActivityMainBinding
+import okhttp3.OkHttpClient
+import okhttp3.Request
+import java.io.IOException
+import kotlin.concurrent.thread
+
+private const val API_URL = "http://192.168.2.73:8000/videos"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
 
     private lateinit var player: SimpleExoPlayer
+    private lateinit var client: OkHttpClient
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,5 +27,25 @@ class MainActivity : AppCompatActivity() {
         // Initialize Player
         player = SimpleExoPlayer.Builder(this).build()
         bind.player.player = player
+
+        // Initialize OkHttp
+        client = OkHttpClient()
+        thread {
+            run()
+        }
+    }
+
+    private fun run() {
+        val request = Request.Builder()
+            .url(API_URL)
+            .build()
+
+        client.newCall(request).execute().use { response ->
+            if (response.isSuccessful) {
+                Log.i("response", response.body!!.string())
+            } else {
+                throw IOException("Unexpected code $response")
+            }
+        }
     }
 }
