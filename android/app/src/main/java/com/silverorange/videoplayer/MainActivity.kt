740be413e12255ca709104015bf03b58922669f6
Sai Vikranth Desu <vikranthdesu@gmail.com>
2021-02-09 20:09:26 -0500

    Add required dependencies

--- android/app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
+++ android/app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
@@ -2,14 +2,14 @@ package com.silverorange.videoplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.silverorange.videoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     private lateinit var bind: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        bind = ActivityMainBinding.inflate(layoutInflater)
-        val view = bind.root
-        setContentView(view)
+        bind = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
