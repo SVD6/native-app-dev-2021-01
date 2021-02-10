86d62a76218071b4569cf3fdad3979a3e44e0e8f
Sai Vikranth Desu <vikranthdesu@gmail.com>
2021-02-09 19:56:13 -0500

    Add dimensions.xml and enable data binding

--- android/app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
+++ android/app/src/main/java/com/silverorange/videoplayer/MainActivity.kt
@@ -2,10 +2,14 @@ package com.silverorange.videoplayer
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.silverorange.videoplayer.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bind: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bind = ActivityMainBinding.inflate(layoutInflater)
+        val view = bind.root
+        setContentView(view)
     }
 }
